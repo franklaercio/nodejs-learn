00bea229f72c482d82bfe97dd6546034e8ebcec6
Frank <franklaercio@gmail.com>
2022-03-10 00:33:13 -0300

    Adding endpoint to create category

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -1,14 +1,10 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/cartegories.routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" });
-});
+app.use(express.json());
 
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-  return response.json({ name: name });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
