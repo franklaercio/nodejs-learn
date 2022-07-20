cf2d37ed44da2a3c2fe2a7b287418544aaaece3a
Frank <franklaercio@gmail.com>
2022-07-20 17:16:33 -0300

    feat: adding user controller

--- rentalx/src/shared/container/index.ts
+++ rentalx/src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from "tsyringe";
 
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { ICategoriesRepository } from "../../modules/cars/respositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/respositories/implementation/CategoriesRepository";
 import { SpecificationRepository } from "../../modules/cars/respositories/implementation/SpecificationRepository";
@@ -13,6 +15,11 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationRepository>(
   "SpecificationRepository",
   SpecificationRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 );
 
 export default container;
