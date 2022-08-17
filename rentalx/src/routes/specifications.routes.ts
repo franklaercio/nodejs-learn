d41d6e7f297c9ebd1695637f38202da441173b61
Frank <franklaercio@gmail.com>
2022-08-16 22:20:48 -0300

    feat: adding middleware token

--- rentalx/src/routes/specifications.routes.ts
+++ rentalx/src/routes/specifications.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 
+import { ensureAtheticated } from "../middlewares/EnsureAtheticated";
 import { CreateSpecificationController } from "../modules/cars/usecases/createSpecification/CreateSpecificationController";
 
 const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
+
+specificationRoutes.use(ensureAtheticated);
 
 specificationRoutes.post("/", createSpecificationController.handle);
 
