38c6463f6a8529d44ff1715e8939b4ff275d2bc7
Frank <franklaercio@gmail.com>
2022-08-24 22:26:16 -0300

    feat: adding categories usecase test

---
+++ rentalx/src/modules/cars/usecases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,50 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../respositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategory: CreateCategoryUseCase;
+let categoriesRepository: CategoriesRepositoryInMemory;
+
+describe("Create category", () => {
+  beforeEach(() => {
+    categoriesRepository = new CategoriesRepositoryInMemory();
+    createCategory = new CreateCategoryUseCase(categoriesRepository);
+  });
+
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "test",
+      description: "test description",
+    };
+
+    await createCategory.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    const categoryExpected = await categoriesRepository.findByName(
+      category.name
+    );
+
+    expect(categoryExpected).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with name exist", async () => {
+    expect(async () => {
+      const category = {
+        name: "test",
+        description: "test description",
+      };
+
+      await createCategory.execute({
+        name: category.name,
+        description: category.description,
+      });
+
+      await createCategory.execute({
+        name: category.name,
+        description: category.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
